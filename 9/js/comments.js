356a3801fb216f2c4a1cfbcc74327af547c6efd2
Keks <keks@htmlacademy.ru>
2024-09-01 22:45:04 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/comments.js
@@ -0,0 +1,62 @@
+import {
+  AVATAR_COUNT_MAX,
+  COMMENT_COUNT_QUANTIFIER,
+  PHOTOS_COUNT_MAX,
+  NAMES,
+  COMMENT_MESSAGES,
+} from "./data";
+
+export function generateAvatar() {
+  const avatar = `img/avatar-${
+    Math.floor(Math.random() * AVATAR_COUNT_MAX) + 1
+  }.svg`;
+  return avatar;
+}
+
+export function generateMessage() {
+  const message =
+    COMMENT_MESSAGES[Math.floor(Math.random() * COMMENT_MESSAGES.length)];
+  return message;
+}
+
+export function generateName() {
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  return name;
+}
+
+export function generateComments() {
+  const comments = [];
+
+  const commentCount = Math.floor(Math.random() * COMMENT_COUNT_QUANTIFIER);
+  for (let commentId = 1; commentId <= commentCount; commentId++) {
+    const avatar = generateAvatar();
+    const message = generateMessage();
+    const name = generateName();
+    const comment = {
+      id: commentId,
+      avatar: avatar,
+      message: message,
+      name: name,
+    };
+    comments.push(comment);
+  }
+  return comments;
+}
+
+export function generatePhotos() {
+  const photos = [];
+
+  for (let i = 1; i <= PHOTOS_COUNT_MAX; i++) {
+    const photo = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `Описание фотографии ${i}`,
+      likes: Math.floor(Math.random() * 186) + 15,
+      comments: generateComments(),
+    };
+
+    photos.push(photo);
+  }
+
+  return photos;
+}
